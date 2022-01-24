b75cab33b88ab85b9f134d1ac4ade09b1af2ca79
Daniel !Gao-!gaseb <91456983+Agabe-gaogaseb07@users.noreply.github.com>
2022-01-24 10:34:57 +0200

    Life in weeks programs
    A program that calculates how many days, months and weeks you have left till you're 90 years old.

---
+++ Life in weeks.py
@@ -0,0 +1,15 @@
+age = input("What is your current age?: ")
+{"90 years; 32,850 days"}
+daysinayear = 365
+current_year = 2022
+year = 2090
+client_age_in_days = int(age) * int(365)
+days = client_age_in_days
+weeks = 32850 // 7
+weeks2 = 32850 - weeks 
+weeks3 = int(age) * int(weeks2)
+months = int(32850) / int(30)
+months2 = 32850 - months
+months3 = int(age) * months2
+
+print(f"You have {days} days, {weeks} weeks and {months} months left.")
